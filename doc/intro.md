bb5cbe38db01e0235ef93b5d595041f60207227a
David J. Lee <deejayessel@gmail.com>
2019-10-09 22:14:04 -0400

    lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rizk
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rizk/core_test.clj
@@ -0,0 +1,7 @@
+(ns rizk.core-test
+  (:require [clojure.test :refer :all]
+            [rizk.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
