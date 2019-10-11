3cbd67500391deca385c58db2c64c6ac3dd8e9ae
David J. Lee <deejayessel@gmail.com>
2019-10-10 22:09:51 -0400

    create-empty-state

--- src/rizk/construct.clj
+++ src/rizk/construct.clj
@@ -1,35 +1,38 @@
-(ns rizk.construct)
+(ns rizk.construct
+  (:require [rizk.definitions :refer [get-definitions]]
+            [ysera.test :refer [is= is is-not error?]]))
 
 (defn create-empty-state
   {:test (fn []
-           (is= (create-empty-state)
+           (is= (create-empty-state 2)
                 {:player-in-turn 1
-                 :players        {1 {:territories ["Alaska"
-                                                   "Alberta"
-                                                   "Ontario"]
-                                     :cards       {:a 1
-                                                   :b 2
-                                                   :c 1}}
-                                  2 {:territories ["Northwest Territory"
-                                                   "Greenland"
-                                                   "Quebec"]
+                 :seed           0
+                 :players        {1 {:id          1
+                                     :territories []
                                      :cards       {:a 0
                                                    :b 0
                                                    :c 0}}
-                                  3 {:territories ["Western United States"
-                                                   "Eastern United States"
-                                                   "Central America"]
-                                     :cards       {:a 1
+                                  2 {:id          2
+                                     :territories []
+                                     :cards       {:a 0
                                                    :b 0
                                                    :c 0}}}
-
-                 :map            {"Alaska" {:name        "Alaska"
-                                            :owner       1
-                                            :troop-count 1}
-                                  }
+                 :map            {}
                  :rules          {:initial-army-size          20
                                   :initial-reinforcement-size 5
-                                  :initial-card-exchange-rate 4
-                                  }
-                 }))}
-  [])
+                                  :initial-card-exchange-rate 4}}))}
+  [num-players]
+  {:player-in-turn 1
+   :seed           0
+   :players        (->> (range 1 (+ num-players 1))
+                        (map-indexed (fn [index player-num]
+                                       {:id          player-num
+                                        :territories []
+                                        :cards       {:a 0 :b 0 :c 0}}))
+                        (reduce (fn [a v]
+                                  (assoc a (:id v) v))
+                                {}))
+   :map            {}
+   :rules          {:initial-army-size          20
+                    :initial-reinforcement-size 5
+                    :initial-card-exchange-rate 4}})
