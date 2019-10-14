74a81b8e8373c5e0cdc18057d96cf39521a67f8d
Garett Tok Ern Liang <garettok@hotmail.com>
2019-10-14 18:14:08 -0400

    valid trade

--- src/rizk/core.clj
+++ src/rizk/core.clj
@@ -4,7 +4,19 @@
                                       get-tile-defn]]
             [rizk.construct :refer [get-tiles]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn valid-trade?
+  "Checks if a set of cards forms a valid trade.  Players trade in hands of 3 cards:
+   a hand must have either one card of each type (i.e., `A-B-C`) or three cards of 
+   the same type (e.g. `A-A-A`)"
+  {:test (fn []
+           (is (valid-trade? {:a 1 :b 1 :c 1}))
+           (is (valid-trade? {:a 3 :b 0 :c 0}))
+           (is-not (valid-trade? {:a 2 :b 1 :c 1})))}
+  [{a :a b :b c :c}]
+  {:pre [(not (neg-int? a)) (not (neg-int? b)) (not (neg-int? c))]}
+  (if (= (+ a b c) 3)
+    (or (= a 3)
+        (= b 3)
+        (= c 3)
+        (and (= a 1) (= b 1) (= c 1)))
+    nil))
